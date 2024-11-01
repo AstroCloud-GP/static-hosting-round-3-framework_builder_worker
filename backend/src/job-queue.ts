50460e7ccecd325d84782acd225e5b9621e00412
NightKnight <Ahmedatwa866@yahoo.com>
2024-11-01 14:19:53 +0200

    Add Redis configuration and update job queues to use it

--- backend/src/job-queue.ts
+++ backend/src/job-queue.ts
@@ -1,12 +1,8 @@
 import { Queue } from "bullmq";
 import { BUILD_JOB_QUEUE_NAME, BuildJob } from "../../shared-code/queues";
+import { redisConfig } from "./config";
 
-const jobQueue = new Queue<BuildJob>(BUILD_JOB_QUEUE_NAME, {
-    connection: {
-        host: "localhost",
-        port: 6379
-    }
-})
+const jobQueue = new Queue<BuildJob>(BUILD_JOB_QUEUE_NAME, { connection: redisConfig })
 
 export default async function addBuildJob(job: BuildJob) {
     const response = await jobQueue.add(`build-${job.project_id}-${job.build_number}`, {
--- worker/src/queues.ts
+++ worker/src/queues.ts
@@ -1,17 +1,13 @@
 import { Queue, Worker } from "bullmq";
 import { BUILD_RESULT_QUEUE_NAME, BUILD_JOB_QUEUE_NAME, BuildResult, BuildJob } from "../../shared-code/queues";
+import { redisConfig } from "./config";
 
-const buildResultQueue = new Queue<BuildResult>(BUILD_RESULT_QUEUE_NAME, {
-    connection: {
-        host: "localhost",
-        port: 6379
-    }
-})
+const buildResultQueue = new Queue<BuildResult>(BUILD_RESULT_QUEUE_NAME, { connection: redisConfig })
 
 export const buildJobWorker = new Worker<BuildJob>(BUILD_JOB_QUEUE_NAME, async (job) => {
     console.log(`Building project ${job.data.project_id} with build number ${job.data.build_number}`);
 
-    
+
     // Simulate a build time
     await new Promise((resolve) => setTimeout(resolve, 5000));
     console.log(`Finished building project ${job.data.project_id} with build number ${job.data.build_number}`);
@@ -23,9 +19,4 @@ export const buildJobWorker = new Worker<BuildJob>(BUILD_JOB_QUEUE_NAME, async (
         logs: "Build completed successfully",
     })
 
-}, {
-    connection: {
-        host: "localhost",
-        port: 6379
-    }
-})
+}, { connection: redisConfig })
