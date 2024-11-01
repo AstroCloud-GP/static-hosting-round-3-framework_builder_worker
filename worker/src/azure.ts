ea048bac2711b4f51a443b8726bddeeb2969b43f
NightKnight <Ahmedatwa866@yahoo.com>
2024-11-01 15:10:48 +0200

    Add Azure Blob Storage integration and configuration

---
+++ worker/src/azure.ts
@@ -0,0 +1,63 @@
+import { DefaultAzureCredential } from "@azure/identity";
+import { BlobServiceClient } from "@azure/storage-blob";
+import { AZURE_STORAGE_ACCOUNT_NAME } from "./config";
+
+const blobService = new BlobServiceClient(`https://${AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, new DefaultAzureCredential())
+
+async function createContainer(containerName: string) {
+    const containerClient = blobService.getContainerClient(containerName)
+    
+    const createContainerResponse = await containerClient.create()
+
+    console.log(`Create container ${containerName} successfully`, createContainerResponse.requestId)
+}
+
+async function doesContainerExist(containerName: string) {
+    const containerClient = blobService.getContainerClient(containerName)
+
+    return await containerClient.exists()
+}
+
+async function uploadToContainer(containerName: string, blobName: string, file: Buffer) {
+    const containerClient = blobService.getContainerClient(containerName)
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName)
+
+    const uploadBlobResponse = await blockBlobClient.upload(file, file.length)
+
+    console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId)
+}
+
+async function emptyContainer(containerName: string) {
+    const containerClient = blobService.getContainerClient(containerName)
+
+    for await (const blob of containerClient.listBlobsFlat()) {
+        await containerClient.deleteBlob(blob.name)
+    }
+
+    console.log(`Emptied container ${containerName}`)
+}
+
+export const AzureService = {
+    createContainer,
+    doesContainerExist,
+    uploadToContainer,
+    emptyContainer
+}
+
+async function testAzure() {
+    const containerName = "container1";
+    console.log("Checking if container exists...")
+
+  if(await AzureService.doesContainerExist(containerName)) {
+    console.log("Container already exists")
+    await AzureService.emptyContainer(containerName)
+    console.log("Container emptied")
+  } else {
+    console.log("Creating container...")
+    await AzureService.createContainer(containerName)
+  }
+
+  const randomFileName = `file-${Math.random().toString(36).substring(7)}.txt`
+  AzureService.uploadToContainer(containerName, randomFileName, Buffer.from("Hello, Azure!"))
+  console.log(`Uploaded ${randomFileName} to container1`)
+}
--- worker/src/config.ts
+++ worker/src/config.ts
@@ -3,3 +3,5 @@ export const redisConfig = {
     host: process.env.REDIS_HOST || "localhost",
     port: parseInt(process.env.REDIS_PORT || "6379"),
 }
+
+export const AZURE_STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME || "atwaa";
