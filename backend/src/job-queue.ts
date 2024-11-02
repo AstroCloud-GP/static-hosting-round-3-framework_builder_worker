139df9339081cf7f2decaa6ad06040e48b2c72bf
NightKnight <Ahmedatwa866@yahoo.com>
2024-11-02 16:21:11 +0200

    update Dockerfile to include public directory and simplify job queue job addition

--- backend/src/job-queue.ts
+++ backend/src/job-queue.ts
@@ -5,14 +5,7 @@ import { redisConfig } from "./config";
 const jobQueue = new Queue<BuildJob>(BUILD_JOB_QUEUE_NAME, { connection: redisConfig })
 
 export default async function addBuildJob(job: BuildJob) {
-    const response = await jobQueue.add(`build-${job.project_id}-${job.build_number}`, {
-        project_id: job.project_id,
-        build_number: job.build_number,
-        project_config: job.project_config,
-        github_token: job.github_token,
-        github_url: job.github_url,
-        container_name: job.container_name
-    })
+    const response = await jobQueue.add(`build-${job.project_id}-${job.build_number}`, job)
 
     console.log("Job added to jobQueue with id: ", response.id)
 }
