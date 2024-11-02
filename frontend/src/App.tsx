afad7f0068b244e19e427ab6d8b3cb1370d6ac30
Ahmed <ahmedsherif602@gmail.com>
2024-11-02 03:26:57 +0200

    static hosting page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,30 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center p-20'>
+
+        <div className='font-bold text-3xl text-white text-center p-5 bg-red-700 rounded-md cursor-pointer hover:bg-red-800 hover:translate-x-0.5 hover:translate-y-0.5 duration-300'>Static Hosting</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- frontend/src/pages/StaticHosting.tsx
+++ frontend/src/pages/StaticHosting.tsx
@@ -2,10 +2,92 @@ import React from 'react'
 
 function StaticHosting() {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+        <div className=''>
+            <div className='p-20'>
+                <div className='text-2xl font-semibold'>Deploy a static site</div>
+                <div className='flex flex-col gap-10 pt-14 w-full'>
+                    <div className='grid grid-cols-3 w-full'>
+                        <div className='text-xl col-span-1 font-semibold'>Source Code</div>
+                        <div className='col-span-2 flex flex-col gap-2'>
+                            <div className='border-md border-solid border p-2'>
+                                <input type="text" placeholder='GitHub Repository' className='w-full outline-none'/>
+                            </div>
+                            <button className='bg-black text-xl text-white p-2 flex flex-row ml-auto'>Connect</button>
+                        </div>
+                    </div>
+                    <form className='flex flex-col gap-10' action="">
+                        <div className='flex flex-row justify-center items-center grid grid-cols-3'>
+                            <div className='col-span-1'>
+                                <div className='text-xl font-semibold'>Name</div>
+                                <p className='text-zinc-500'>A unique name for the static site</p>
+                            </div>
+                            <div className='border-md col-span-2 border-solid border'>
+                                <input type="text" placeholder='site name' className='w-full p-2'/>
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-3 flex flex-row items-center'>
+                            <div className='col-span-1'>
+                                <div className='text-xl font-semibold'>Branch</div>
+                                <p className='text-zinc-500'>Which Git branch to build and deploy</p>
+                            </div>
+                            <div className='border-md col-span-2 border-solid border'>
+                                <input type="text" placeholder='branch' className='w-full p-2'/>
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-3  flex flex-row items-center'>
+                            <div className='col-span-1'>
+                                <div className='flex flex-row gap-5 items-center'>
+                                    <div className='text-xl font-semibold'>Root Directory</div>
+                                    <div className='text-md text-zinc-400'>Optional</div>
+                                </div>
+                                <p className='text-zinc-500'>Commands will run from this directory rather than the repository root </p>
+                            </div>
+                            <div className='border-md col-span-2 border-solid border'>
+                                <input type="text" placeholder='e.g. src' className='w-full p-2'/>
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-3 flex flex-row items-center'>
+                            <div className='col-span-1'>
+                                <div className='text-xl font-semibold'>Build Command</div>
+                                {/* <p className='text-zinc-500'></p> */}
+                            </div>
+                            <div className='border-md col-span-2 border-solid border'>
+                                <input type="text" placeholder='e.g. npm install' className='w-full p-2'/>
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-3 flex flex-row items-center'>
+                            <div className='col-span-1'>
+                                <div className='text-xl font-semibold'>Publish directory</div>
+                                <p className='text-zinc-500'>The relative path of the directory containing built assets to publish. Examples: ./, ./build, dist and frontend/build.</p>
+                            </div>
+                            <div className='border-md col-span-2 border-solid border'>
+                                <input type="text" placeholder='' className='w-full p-2'/>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-5 border p-6'>
+                            <div className='flex flex-col'>
+                                <div className='text-2xl font-semibold'>Environment variables</div>
+                                <p className='text-zinc-500'>Set environment-specific config and secrets (such as API keys), then read those values from your code.</p>
+                            </div>
+                            <div className='flex flex-col gap-5'>
+                                <div className='flex flex-row gap-4'>
+                                    <div className='border basis-1/2'>
+                                        <input type="text" placeholder='NAME_OF_VARIABLE' className='w-full h-full p-2'/>
+                                    </div>
+                                    <div className='border basis-1/2'>
+                                        <input type="text" placeholder='value' className='w-full h-full p-2'/>
+                                    </div>
+                                    <button className='bg-red-500 text-white p-3 font-semibold' onClick={e=>e.preventDefault()}>Delete</button>
+                                </div>
+                            </div>
+                            <div>
+                                <button className='border p-3 hover:bg-black hover:text-white' onClick={e=>e.preventDefault()}>+ Add Environment Variable</button>
+                            </div>
+                        </div>
+                        <input type="submit" value={"Deploy"} className='cursor-pointer bg-black text-white text-2xl mr-auto p-2'/>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
