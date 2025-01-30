e5000066356584f67f72dab8299a2234639ff073
NightKnight <Ahmedatwa866@yahoo.com>
2025-01-30 15:39:10 +0200

    update Dockerfile and docker-compose for Node version upgrade; modify entrypoint command; add Docker connection check in worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,3 +1,4 @@
+import { execSync } from "child_process";
 import { buildJobWorker } from "./queues";
 
 async function main() {
@@ -8,6 +9,14 @@ async function main() {
   buildJobWorker.on("failed", (job, err) => {
     console.error(`Build job ${job?.id} failed with error: ${err.message}`);
   });
+
+  // Check if Docker is running
+  try {
+    execSync(`docker ps`)
+  } catch {
+    console.error("Failed to connect to Docker. Shutting down.");
+    process.exit(1);
+  }
 
   console.log("Worker is running...");
 }
